1ed699d0af7cff223baadc9a5f1e2e9f55a6d8b7
Jeff Harvey-Smith <jharveysmith@yugabyte.com>
2021-07-16 15:40:46 -0700

    Fix shellshock errors

--- src/create_venv.sh
+++ src/create_venv.sh
@@ -48,6 +48,7 @@ function text_file_sha() {
   local file="${1}"
   local tmp
   tmp="$(sort -u <<<"$(grep -v '^#' "${file}")")"
+  # shellcheck disable=SC2154
   awk '{print $1}'<<<"$(${yb_sha256sum} <<<"${tmp}")"
 }
 
